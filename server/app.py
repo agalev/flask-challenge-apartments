ec19bbd0ac239473f629047b91180b1124b1ce37
Alexander <alex.galev@yahoo.com>
2023-04-12 10:26:59 -0600

    solved

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db
+from models import db, Apartment, Tenant, Lease
 
 app = Flask( __name__ )
 app.config[ 'SQLALCHEMY_DATABASE_URI' ] = 'sqlite:///apartments.db'
@@ -11,6 +11,98 @@ app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS' ] = False
 migrate = Migrate( app, db )
 db.init_app( app )
 
+api = Api(app)
+
+class ApartmentsController(Resource):
+    def get(self):
+        return [apartment.to_dict() for apartment in Apartment.query.all()], 200
+    def post(self):
+        new_apartment = Apartment(number=request.get_json()['number'])
+        db.session.add(new_apartment)
+        db.session.commit()
+        return new_apartment.to_dict(), 201
+api.add_resource(ApartmentsController, '/apartments')
+
+class ApartmentControllerByID(Resource):
+    def patch(self, id):
+        try:
+            apartment = Apartment.query.get(id)
+            apartment.number = request.get_json()['number']
+            db.session.commit()
+            return apartment.to_dict(), 200
+        except Exception:
+            return {'error': 'Could not update specified apartment'}, 404
+    
+    def delete(self, id):
+        try:
+            apartment = Apartment.query.get(id)
+            db.session.delete(apartment)
+            db.session.commit()
+            return '', 204
+        except Exception:
+            return {'error': 'Could not delete specified apartment'}, 404
+api.add_resource(ApartmentControllerByID, '/apartments/<int:id>')
+
+class TenantsController(Resource):
+    def get(self):
+        return [tenant.to_dict() for tenant in Tenant.query.all()], 200
+    def post(self):
+        new_tenant = Tenant(name=request.get_json()['name'], age=request.get_json()['age'])
+        db.session.add(new_tenant)
+        db.session.commit()
+        return new_tenant.to_dict(), 201
+api.add_resource(TenantsController, '/tenants')
+
+class TenantControllerByID(Resource):
+    def patch(self, id):
+        try:
+            tenant = Tenant.query.get(id)
+            tenant.name = request.get_json()['name']
+            tenant.age = request.get_json()['age']
+            db.session.commit()
+            return tenant.to_dict(), 200
+        except Exception:
+            return {'error': 'Could not update specified tenant'}, 404
+    
+    def delete(self, id):
+        try:
+            tenant = Tenant.query.get(id)
+            db.session.delete(tenant)
+            db.session.commit()
+            return '', 204
+        except Exception:
+            return {'error': 'Could not delete specified tenant'}, 404
+api.add_resource(TenantControllerByID, '/tenants/<int:id>')
+
+class LeasesController(Resource):
+    def get(self):
+        return [lease.to_dict() for lease in Lease.query.all()], 200
+    def post(self):
+        new_lease = Lease(rent=request.get_json()['rent'], apartment_id=request.get_json()['apartment_id'], tenant_id=request.get_json()['tenant_id'])
+        db.session.add(new_lease)
+        db.session.commit()
+        return new_lease.to_dict(), 201
+api.add_resource(LeasesController, '/leases')
+
+class LeaseControllerByID(Resource):
+    def patch(self, id):
+        try:
+            lease = Lease.query.get(id)
+            lease.rent = request.get_json()['rent']
+            db.session.commit()
+            return lease.to_dict(), 200
+        except Exception:
+            return {'error': 'Could not update specified lease'}, 404
+    
+    def delete(self, id):
+        try:
+            lease = Lease.query.get(id)
+            db.session.delete(lease)
+            db.session.commit()
+            return '', 204
+        except Exception:
+            return {'error': 'Could not delete specified lease'}, 404
+api.add_resource(LeaseControllerByID, '/leases/<int:id>')
 
 if __name__ == '__main__':
-    app.run( port = 3000, debug = True )
+    app.run( port = 5555, debug = True )
--- server/models.py
+++ server/models.py
@@ -3,5 +3,59 @@ from flask_restful import Api, Resource
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class Apartment(db.Model, SerializerMixin):
+    __tablename__ = 'apartments'
+
+    serialize_rules = ('-leases.apartment',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    leases = db.relationship('Lease', backref='apartment', cascade='all, delete, delete-orphan')
+
+    @validates('number')
+    def validate_number(self, key, number):
+        if number < 0:
+            raise ValueError("number must be higher than 0")
+        return number
+    
+class Tenant(db.Model, SerializerMixin):
+    __tablename__ = 'tenants'
+
+    serialize_rules = ('-leases.tenant',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    leases = db.relationship('Lease', backref='tenant', cascade='all, delete, delete-orphan')
+
+    @validates('age')
+    def validate_age(self, key, age):
+        if age < 18:
+            raise ValueError("Age must be higher than 18")
+        return age
+
+class Lease(db.Model, SerializerMixin):
+    __tablename__ = 'leases'
+
+    serialize_rules = ('-apartment.leases', '-tenant.leases',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    apartment_id = db.Column(db.Integer, db.ForeignKey('apartments.id'))
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'))
+    rent = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
